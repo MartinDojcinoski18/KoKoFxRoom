4986870ae8665bbad9700606091a8a210b8e3ccc
Martin Dojcinoski <dojcinoskimartin4@gmail.com>
2026-02-14 23:30:45 +0100

    feat: Load and expose API_KEY from environment
    Exposes the `API_KEY` environment variable to the client-side application for use in API calls. This change also configures Netlify for proper deployment and routing.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': process.env
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, (process as any).cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      // Safely expose ONLY the API_KEY to the client-side code
+      'process.env.API_KEY': JSON.stringify(env.API_KEY)
+    }
   }
 })
